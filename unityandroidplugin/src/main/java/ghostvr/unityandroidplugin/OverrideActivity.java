00cbc7dc100659af54ec19e0788ab479d1b9e98c
Divyanshu <divyanshu.pundir@gmail.com>
2017-06-07 22:26:13 +0530

    feat: Initial Commit

---
+++ unityandroidplugin/src/main/java/ghostvr/unityandroidplugin/OverrideActivity.java
@@ -0,0 +1,33 @@
+package ghostvr.unityandroidplugin;
+
+import android.os.Bundle;
+import com.unity3d.player.UnityPlayerActivity;
+
+public class OverrideActivity extends UnityPlayerActivity {
+
+    RotationSensorPlugin rotationSensorPlugin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        rotationSensorPlugin = new RotationSensorPlugin(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (rotationSensorPlugin.isRotationSensorEnabled() && rotationSensorPlugin.getRotationSensor() != null){
+            rotationSensorPlugin.registerListener();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (rotationSensorPlugin.getRotationSensor() != null){
+            rotationSensorPlugin.unregisterListener();
+        }
+    }
+}
---
+++ unityandroidplugin/src/main/java/ghostvr/unityandroidplugin/RotationSensorPlugin.java
@@ -0,0 +1,92 @@
+package ghostvr.unityandroidplugin;
+
+/**
+ * Created by Divyanshu on 6/7/17.
+ */
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.SystemClock;
+import android.util.Log;
+
+import com.unity3d.player.UnityPlayerActivity;
+
+public class RotationSensorPlugin implements SensorEventListener {
+    private UnityPlayerActivity upa;
+    private SensorManager sensorManager;
+    private Sensor rotationSensor;
+    private float[] rotationVector;
+    private float[] rotationMatrix;
+    private float[] orientationMatrix;
+    private float[] quatMatrix;
+
+
+    private boolean isRotationSensorEnabled = false;
+
+    public RotationSensorPlugin(UnityPlayerActivity unityPlayerActivity) {
+        upa = unityPlayerActivity;
+        sensorManager = (SensorManager) upa.getSystemService(Context.SENSOR_SERVICE);
+        rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        rotationVector = new float[3];
+        rotationMatrix = new  float[16];
+        orientationMatrix = new float[3];
+        quatMatrix = new float[4];
+        isRotationSensorEnabled = true;
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR){
+            System.arraycopy(event.values, 0, rotationVector, 0, rotationVector.length);
+        }
+
+        Log.d("Unity Sensor Plugin", String.valueOf(SystemClock.currentThreadTimeMillis()));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+
+    public void registerListener() {
+        sensorManager.registerListener(this, rotationSensor, SensorManager.SENSOR_DELAY_GAME);
+        isRotationSensorEnabled = true;
+    }
+
+    public void unregisterListener() {
+        sensorManager.unregisterListener(this);
+        isRotationSensorEnabled = false;
+    }
+
+    public boolean isRotationSensorEnabled() {
+        return isRotationSensorEnabled;
+    }
+
+    public Sensor getRotationSensor() {
+        return rotationSensor;
+    }
+
+    public float[] getRotationVector(){
+        return rotationVector;
+    }
+
+    public float[] getRotationMatrix(){
+        SensorManager.getRotationMatrixFromVector(rotationMatrix, rotationVector);
+        return rotationMatrix;
+    }
+
+    public float[] getOrientationMatrix(){
+        getRotationMatrix();
+        SensorManager.getOrientation(rotationMatrix, orientationMatrix);
+        return orientationMatrix;
+    }
+
+    public float[] getQuatMatrix(){
+        SensorManager.getQuaternionFromVector(quatMatrix, rotationVector);
+        return quatMatrix;
+    }
+}
